a72b159b5d1765cc3dec38c344d0e05fff331af5
Viraj <virajjadhao85@gmail.com>
2023-04-05 20:12:44 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 function App() {
-  return (
+  const [newsList,setNewsList] = useState([])
+  useEffect(()=>{
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+    .then((response)=>response.json()).then((data)=>{
+      setNewsList(data)
+    })
+  }, [])
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Space News</h1>
+      </div>
+      <div className="newsContainer">
+        {
+          newsList.map((val,key)=>{
+            return(
+              <div className='article' key={key} onClick={()=>{window.location.href = val.url}}>
+                <h2>{val.title}</h2>
+                <img src={val.imageUrl}/>
+                <h4>{val.summary}</h4>
+                <h2>{val.publishedAt}</h2>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
